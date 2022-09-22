7c1a0b2b6526d85f82be640dad59c95da37015d7
Brandon Tetrick <brandontetrick@Brandons-MacBook-Air.local>
2022-09-22 16:58:02 -0600

    9/22/22

---
+++ components/layout.js
@@ -0,0 +1,22 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import ResponsiveDrawer from "./drawer";
+import Toolbar from '@mui/material/Toolbar';
+
+
+export default function Layout({ children }) {
+
+    return (
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+
+        {/* Responsive Drawer */}
+        <ResponsiveDrawer />
+
+        <Box>
+            <Toolbar /> 
+            {children}
+        </Box>
+      </Box>
+    )
+  }
