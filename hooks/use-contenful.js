a1e7aca8cb935ba3cfc0e6bcd570cf72d5e9fd99
Brandon Tetrick <brandontetrick@Brandons-MacBook-Air.local>
2022-10-19 16:44:59 -0600

    set up error handling on API

--- hooks/use-contenful.js
+++ hooks/use-contenful.js
@@ -1,24 +1,29 @@
+import * as React from 'react';
 import {useState, useEffect} from "react";
 
 export default function useContentful(query) {
     let [data, setData] = useState(null); 
+    let [errors, setErrors] = useState(null); 
 
     useEffect(() => {
         window
-        .fetch(`https://graphql.contentful.com/content/v1/spaces/vpe0m1tts7x2/`, {
+        .fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID}`, {
             method: "POST",
             headers: {
             "Content-Type": "application/json",
             // Authenticate the request
-            Authorization: "Bearer -qnkhZAtm88ji09_T5MW1nRRuA6DWJ4n8Uet86mOHxs",
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_CDA_TOKEN}`,
             },
             // send the GraphQL query
             body: JSON.stringify({ query }),
         })
         .then((response) => response.json())
-        .then((json => setData(json.data)))
+        .then(({data, errors}) => {
+            if (errors) setErrors(errors)
+            if (data)setData(data); 
+        })
+        .catch(error => setErrors([error])); 
     }, [query]);
 
-    console.log(data)
-    return {data}
+    return {data, errors}
 }
--- next.config.js
+++ next.config.js
@@ -4,4 +4,10 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  env: {
+    // declare here all your variables
+    NEXT_PUBLIC_CONTENTFUL_SPACE_ID: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+    NEXT_PUBLIC_CDA_TOKEN: process.env.NEXT_PUBLIC_CDA_TOKEN,
+  }
+}
