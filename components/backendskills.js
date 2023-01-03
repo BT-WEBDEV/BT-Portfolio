f5fbd6ed8b4edd25586805a8f1dbba175de4ce0b
Brandon Tetrick <brandontetrick@Brandons-MacBook-Air.local>
2023-01-03 15:40:31 -0700

    finally got dynamic pages down

--- components/backendskills.js
+++ components/backendskills.js
@@ -1,43 +1,58 @@
 import * as React from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
+import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
+import styles from '../styles/skill.module.css'
 
 export default function BackendSkillsHome({data}) {
-  
-    return (
-        <Box
-          sx={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'center', 
-            '& > :not(style)': {
-              m: 1,
-              width: 128,
-              height: 128,
-            },
-          }}
-        >
-        { data.skillsCollection.items.map(skill => {
+  // console.table(data.skillsCollection.items); 
 
-          if(skill.usage == "Backend") {
-            return (
-              <Paper elevation={0} variant="outlined" key={skill.name}>
-                <Typography paragraph align="center">
-                  {skill.name}
-                </Typography>
-              </Paper>
-            ); 
-          }
+  return (
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'center', 
+          '& > :not(style)': {
+            m: 1,
+            width: 128,
+            height: 128,
+          },
+        }}
+      >
+      
+      {/* LOOP THROUGH CONTENTFUL OBJECT & GET FRONTEND SKILLS  */}
+      { data.skillsCollection.items.map(skill => {
+        if(skill.usage == "Backend") {
+          return (
+            <Link 
+              key={skill.slug}
+              href={'/skills/' + skill.slug}
+              className={styles.skillLink}
+              underline="none"
+              style={{
+                backgroundImage: `url(${skill.image.url})`,
+              }}
+            > 
+              <Typography 
+                h3="true"
+                align="center"
+                className = {styles.hoverableShow}
+              >
+                {skill.name}
+              </Typography>
+                
+            </Link>
+          ); 
+        }
 
-        })}
-        </Box>
-    );
+      })}
+      </Box>
+  );
 
 }
-
-
 BackendSkillsHome.propTypes = {
     name:PropTypes.string
 }; 
