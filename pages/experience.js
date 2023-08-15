124469bcf41f97f0f08a778d10a134dcecd8b6b5
Brandon Tetrick <brandontetrick@Brandons-MacBook-Air.local>
2023-08-15 09:12:33 -0600

    updating portfolio experience page

--- pages/experience.js
+++ pages/experience.js
@@ -107,12 +107,11 @@ export default function Experience() {
           <Typography 
             paragraph align="center"
           >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupidatat non proident, 
-            sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Results-driven web developer with over 7 years of experience spanning diverse projects in both public and private sectors. 
+            Specializing in developing dynamic websites that blend creativity and cutting-edge technology. 
+            Adept at implementing strategic digital marketing initiatives that have led to a 25% average increase in website traffic across various projects. 
+            Lifelong learner, dedicated to staying ahead of industry trends and leveraging emerging technologies. 
+            Passionate about web development and digital innovation. Proficient in HTML, CSS, JavaScript, and proficient in collaborating with cross-functional teams to deliver high-quality solutions.
           </Typography>
 
           <Divider/> 
